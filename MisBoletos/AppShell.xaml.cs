54a69bf409c03858a2c9493f249bd164b895c1f2
emiliogf10 <emiliogf10@gmail.com>
2025-08-25 19:20:43 +0200

    Agregar archivos de proyecto.

---
+++ MisBoletos/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MisBoletos
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MisBoletos/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MisBoletos")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MisBoletos.Pages")]
